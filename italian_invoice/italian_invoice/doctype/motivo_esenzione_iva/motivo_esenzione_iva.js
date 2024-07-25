2a7408ab991d6f57f100df6afe833bb61c24fe82
Lorenzo Caldara <lorenzo.caldara@gmail.com>
2024-07-25 06:12:55 +0200

    Doctype motivi esenzione iva

---
+++ italian_invoice/italian_invoice/doctype/motivo_esenzione_iva/motivo_esenzione_iva.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Solede SA and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Motivo esenzione IVA", {
+// 	refresh(frm) {
+
+// 	},
+// });
