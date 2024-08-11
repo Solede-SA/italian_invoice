3324d2b43c4f94f680665fdb47002c9ca0a62f5c
Lorenzo Caldara <lorenzo.caldara@gmail.com>
2024-08-11 15:00:17 +0200

    rivisto blocco se modifico cliente da pa a privato

--- italian_invoice/crud_events/sales_invoice/before_save.py
+++ italian_invoice/crud_events/sales_invoice/before_save.py
@@ -2,20 +2,26 @@ import frappe
 
 
 def execute(doc, method=None):
+    if doc.is_return:
+        return
+
+    if doc.is_new():
+        return
+
     customer = frappe.get_doc("Customer", doc.customer)
-    # if doc.is_return:
-    #     return
+    old_doc = doc.get_doc_before_save()
+    old_customer = frappe.get_doc("Customer", old_doc.customer)
 
-    # if customer.is_public_administration and doc.naming_series != "PAINIV/.YY./":
-    #     frappe.throw(
-    #         (
-    #             "La fattura è stata creata per un utente privato. Puoi midificare il cliente associando solo un altro cliente privato"
-    #         )
-    #     )
+    if old_customer.is_public_administration and not customer.is_public_administration:
+        frappe.throw(
+            (
+                "Non puoi modificare un cliente PA con uno privato. Crea una nuova fattura"
+            )
+        )
 
-    # if not customer.is_public_administration and doc.naming_series != "SINV/.YY./":
-    #     frappe.throw(
-    #         (
-    #             "La fattura è stata creata per un utente pubblico. Puoi midificare il cliente associando solo un altro cliente pubblico"
-    #         )
-    #     )
+    if not old_customer.is_public_administration and customer.is_public_administration:
+        frappe.throw(
+            (
+                "Non puoi modificare un cliente privato con uno PA. Crea una nuova fattura"
+            )
+        )
