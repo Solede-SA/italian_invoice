0c77583594b8ab0f9e3df78fc62addb3b44af490
Lorenzo Caldara <lorenzo.caldara@gmail.com>
2025-03-13 14:29:21 +0100

    Update custom_purchase_invoice.js

--- italian_invoice/public/js/custom_purchase_invoice.js
+++ italian_invoice/public/js/custom_purchase_invoice.js
@@ -45,7 +45,6 @@ frappe.ui.form.on("Purchase Invoice", {
     if (["TD19", "TD17"].find((x) => x == frm.doc.custom_tipo_di_documento)) {
       frm.set_value("taxes_and_charges", "Reverse Charge");
     } else if (frm.doc.custom_tipo_di_documento == "TD18") {
-      console.log("TD18");
       frm.set_value("taxes_and_charges", "IVA acquisti CEE al 22%");
     } else {
       frm.set_value("taxes_and_charges", "");
@@ -62,6 +61,14 @@ frappe.ui.form.on("Purchase Invoice", {
           );
         }
       })
+      if (frm.doc.taxes) {
+        frm.doc.taxes.forEach((tax, idx) => {
+          if (tax.base_tax_amount == 0 && tax.custom_motivo_esenzione_iva === undefined) {
+            frappe.throw(__("<b>Motivo esenzione IVA</b> mancante nella tassa {0}", [idx + 1]));
+          }
+        });
+      }
+      
     }
   },
 });
