03461f3a50629844c40e75b0eddc7fd6c7fc7c07
Lorenzo Caldara <lorenzo.caldara@gmail.com>
2025-05-20 15:34:38 +0200

    Fix banca in fattura elettronica

--- italian_invoice/utilities/fatture.py
+++ italian_invoice/utilities/fatture.py
@@ -251,6 +251,7 @@ def get_invoice_data(doc):
         "tax_data": tax_data,
         "vat_collectability": vat_collectability,
         "payment_schedule": doc.payment_schedule,
+        "custom_bank_account": doc.custom_bank_account,
         "apply_discount_on": doc.apply_discount_on,
         # "stamp_duty": doc.stamp_duty,
     }
